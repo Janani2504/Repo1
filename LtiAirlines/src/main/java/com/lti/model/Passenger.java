632e716cdc44c7c31cabda3d289d916264774586
janan <janan@JANESUJI>
2021-09-15 13:21:22 +0530

    Modified Passenger through br1

--- LtiAirlines/src/main/java/com/lti/model/Passenger.java
+++ LtiAirlines/src/main/java/com/lti/model/Passenger.java
@@ -1,5 +1,42 @@
 package com.lti.model;
 
 public class Passenger {
+	int passengerId;
+	String passengerName;
+	String address;
+	int age;
+
+	public int getPassengerId() {
+		return passengerId;
+	}
+
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	
 
 }
