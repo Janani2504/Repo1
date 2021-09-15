3dddd874938a1860a566d5c7c9361b5ae146b3ff
SaiCharan87s <pickraju24@gmail.com>
2021-09-15 14:52:15 +0530

    Ticket change added

--- LtiAirlines/src/main/java/com/lti/model/Ticket1.java
+++ LtiAirlines/src/main/java/com/lti/model/Ticket1.java
@@ -1,10 +1,34 @@
 package com.lti.model;
 
 public class Ticket1 {
+	 int ticketNo;
+	 int seatNo;
+
+	public Ticket1(int ticketNo, int seatNo) {
+		super();
+		this.ticketNo = ticketNo;
+		this.seatNo = seatNo;
+	}
+
+	public int getTicketNo() {
+		return ticketNo;
+	}
+
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+
+	public int getSeatNo() {
+		return seatNo;
+	}
+
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	   
 	}
 
 }
